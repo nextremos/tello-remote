8b72b3a90d62ec22ab6f9b5dcb1f1371e040795c
Simon Weber <simon.weber@sitewerk.ch>
2018-12-15 23:37:16 +0100

    add different tello ports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
 import dgram from "dgram";
 import { AddressInfo } from "net";
 
+const PORT_COMMAND_SEND_RECEIVE = 8889;
+const PORT_STATE_RECEIVE = 8890;
+const PORT_VIDEO_RECEIVE = 11111;
+
 const server = dgram.createSocket('udp4');
 
 server.on('error', (err) => {
@@ -17,7 +21,16 @@ server.on('listening', () => {
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(41234);
+// todo:sw - bind multiple ports
+server.bind(PORT_STATE_RECEIVE);
 // server listening 0.0.0.0:41234
+
+server.send("Hello", PORT_COMMAND_SEND_RECEIVE, "192.168.1.1", (error: Error | null, bytes: number) => {
+  if (error) {
+    console.log(`Send error ${error}`);
+    return;
+  }
+  console.log(`Send receive data ${bytes}`);
+})
 
 export default server;
