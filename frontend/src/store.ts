d1f83366c851ad58ef16cecc0f0ac63232b1b97e
Simon Weber <simon.weber@sitewerk.ch>
2018-12-16 20:50:27 +0100

    use strict mode in store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,16 +1,11 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  strict: process.env.NODE_ENV !== "production", // henever Vuex state is mutated outside of mutation handlers, an error will be thrown. This ensures that all state mutations can be explicitly tracked by debugging tools.
+  state: {},
+  mutations: {},
+  actions: {}
 });
